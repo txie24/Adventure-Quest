c0b48fd036da927db9f966a7d5ccb6ce707cc40f
Tinyu <txie24@ucsc.edu>
2024-05-22 22:18:02 -0700

    V0.3

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -5,10 +5,12 @@ class Platformer extends Phaser.Scene {
 
     init() {
         // variables and settings
-        this.ACCELERATION = 500;
-        this.DRAG = 700;    // DRAG < ACCELERATION = icy slide
+        this.ACCELERATION = 200;
+        this.DRAG = 1000;    // DRAG < ACCELERATION = icy slide
         this.physics.world.gravity.y = 1500;
-        this.JUMP_VELOCITY = -900;
+        this.JUMP_VELOCITY = -500;
+        this.PARTICLE_VELOCITY = 50;
+        this.SCALE = 2.0;
     }
 
     create() {
@@ -25,61 +27,146 @@ class Platformer extends Phaser.Scene {
         this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
         this.killableLayer = this.map.createLayer("Killables", this.tileset, 0, 0);
         this.overlayLayer = this.map.createLayer("Overlays", this.tileset, 0, 0);
-
-        // Scale the layers
-        this.groundLayer.setScale(2.0);
-        this.killableLayer.setScale(2.0);
-        this.overlayLayer.setScale(2.0);
-
-        // Make the ground layer collidable
+        
+        // Make the ground and killable layers collidable
         this.groundLayer.setCollisionByProperty({ collides: true });
+        this.killableLayer.setCollisionByProperty({ collides: true });
 
-        // Bring the overlays layer to the top
-        this.overlayLayer.setDepth(1);
+        this.coins = this.map.createFromObjects("Objects", {
+            name: "coin",
+            key: "tilemap_sheet",
+            frame: 151
+        });
+        // Since createFromObjects returns an array of regular Sprites, we need to convert 
+        // them into Arcade Physics sprites (STATIC_BODY, so they don't move) 
+        this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
+
+        // Create a Phaser group out of the array this.coins
+        // This will be used for collision detection below.
+        this.coinGroup = this.add.group(this.coins);
 
         // set up player avatar
-        my.sprite.player = this.physics.add.sprite(100, 200, "platformer_characters", "tile_0000.png").setScale(SCALE);
+        my.sprite.player = this.physics.add.sprite(90, 100, "platformer_characters", "tile_0000.png");
         my.sprite.player.setCollideWorldBounds(true);
 
         // Enable collision handling
         this.physics.add.collider(my.sprite.player, this.groundLayer);
 
+        // Handle collision detection with coins
+        this.physics.add.overlap(my.sprite.player, this.coinGroup, (obj1, obj2) => {
+            obj2.destroy(); // remove coin on overlap
+        });
+
+        // Handle collision detection with killable layer
+        this.physics.add.collider(my.sprite.player, this.killableLayer, () => {
+            this.scene.restart(); // Restart the scene when player touches the killable layer
+        });
+
         // set up Phaser-provided cursor key input
         cursors = this.input.keyboard.createCursorKeys();
 
+        this.rKey = this.input.keyboard.addKey('R');
+
         // debug key listener (assigned to D key)
         this.input.keyboard.on('keydown-D', () => {
-            this.physics.world.drawDebug = this.physics.world.drawDebug ? false : true;
-            this.physics.world.debugGraphic.clear();
+            this.physics.world.drawDebug = this.physics.world.drawDebug ? false : true
+            this.physics.world.debugGraphic.clear()
         }, this);
+
+        // Add movement vfx here
+        my.vfx = {};
+        // movement vfx
+        my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
+            frame: ['smoke_03.png', 'smoke_09.png'],
+            scale: { start: 0.03, end: 0.1 },
+            lifespan: 350,
+            alpha: { start: 1, end: 0.1 },
+        });
+        my.vfx.walking.stop();
+
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
+        this.cameras.main.setDeadzone(50, 50);
+        this.cameras.main.setZoom(this.SCALE);
+
+        // Create a container for the moving platforms
+        this.movingPlatformContainer = this.add.container(550, 297);
+
+        // Add individual platform images to the container
+        const platform1 = this.add.image(9, 9, "sprite_tiles", "48").setScale(1);
+        const platform2 = this.add.image(27, 9, "sprite_tiles", "49").setScale(1);
+        const platform3 = this.add.image(45, 9, "sprite_tiles", "50").setScale(1);
+        
+        this.movingPlatformContainer.add([platform1, platform2, platform3]);
+
+        // Enable physics on the container
+        this.physics.world.enable(this.movingPlatformContainer);
+        this.movingPlatformContainer.body.setImmovable(true);
+        this.movingPlatformContainer.body.allowGravity = false;
+        this.movingPlatformContainer.body.setVelocityX(100);
+
+        // Set container size to match the platforms
+        this.movingPlatformContainer.body.setSize(54, 18);
+
+        // Enable collision handling for the moving platform container
+        this.physics.add.collider(my.sprite.player, this.movingPlatformContainer);
+
+        // Set platform boundaries
+        this.platformMinX = 440;
+        this.platformMaxX = 550;
     }
 
     update() {
+        // Player controls
         if (cursors.left.isDown) {
-            // have the player accelerate to the left
-            my.sprite.player.body.setAccelerationX(-this.ACCELERATION);
+            my.sprite.player.setAccelerationX(-this.ACCELERATION);
             my.sprite.player.resetFlip();
             my.sprite.player.anims.play('walk', true);
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth / 2 - 10, my.sprite.player.displayHeight / 2 - 5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            if (my.sprite.player.body.blocked.down) {
+                my.vfx.walking.start();
+            }
+
         } else if (cursors.right.isDown) {
-            // have the player accelerate to the right
-            my.sprite.player.body.setAccelerationX(this.ACCELERATION);
+            my.sprite.player.setAccelerationX(this.ACCELERATION);
             my.sprite.player.setFlip(true, false);
             my.sprite.player.anims.play('walk', true);
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth / 2 + 10, my.sprite.player.displayHeight / 2 - 5, false);
+            my.vfx.walking.setParticleSpeed(-this.PARTICLE_VELOCITY, 0);
+            if (my.sprite.player.body.blocked.down) {
+                my.vfx.walking.start();
+            }
+
         } else {
-            // set acceleration to 0 and have DRAG take over
-            my.sprite.player.body.setAccelerationX(0);
-            my.sprite.player.body.setDragX(this.DRAG);
-            my.sprite.player.anims.play('idle');
+            my.sprite.player.setAccelerationX(0);
+            if (my.sprite.player.body.blocked.down) {
+                my.sprite.player.setVelocityX(0); // Stop player on the ground
+                my.sprite.player.anims.play('idle');
+            }
+            my.vfx.walking.stop();
         }
 
-        // player jump
-        // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
         if (!my.sprite.player.body.blocked.down) {
+            my.sprite.player.setDragX(100); // Increase drag while in air
             my.sprite.player.anims.play('jump');
+        } else {
+            my.sprite.player.setDragX(this.DRAG); // Reset drag when on ground
         }
+
         if (my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
-            // set a Y velocity to have the player "jump" upwards (negative Y direction)
             my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.rKey)) {
+            this.scene.restart();
+        }
+
+        // Update moving platform container
+        if (this.movingPlatformContainer.x >= this.platformMaxX) {
+            this.movingPlatformContainer.body.setVelocityX(-40);
+        } else if (this.movingPlatformContainer.x <= this.platformMinX) {
+            this.movingPlatformContainer.body.setVelocityX(40);
         }
     }
 }
